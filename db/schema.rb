53dacdd470de966a8a8b25c4f5dfa2c04cc2c2f9
Justin Cheung <justinwkcheung@github.com>
2017-02-02 14:30:19 -0500

    Create and migrate guests table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170202191525) do
+ActiveRecord::Schema.define(version: 20170202192157) do
+
+  create_table "guests", force: :cascade do |t|
+    t.string  "first_name"
+    t.string  "last_name"
+    t.text    "dietary_restrictions"
+    t.decimal "salary"
+    t.integer "number_of_kids"
+    t.text    "vulnerabilities"
+    t.text    "iltlnesses"
+    t.text    "medication"
+    t.text    "voting_preferences"
+  end
 
   create_table "parts", force: :cascade do |t|
     t.string  "name"
